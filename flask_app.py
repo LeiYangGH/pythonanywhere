0e74ab2b9da62d843eb61b509a6357c0f35facf3
LeiYangGH <675686066@qq.com>
2024-09-08 11:45:58 +0800

    form

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,19 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask github!'
+@app.route('/',methods = ['POST','GET'])
+def index():
+    # return 'Hello from Flask github!'
+    username = request.form.get('username')
+    password = request.form.get('pw')
+    return render_template("index.html")
 
+@app.route('/res',methods = ['POST','GET'])
+def res():
+    username = request.form.get('username')
+    password = request.form.get('pw')
+    return f"{username} {password}"
